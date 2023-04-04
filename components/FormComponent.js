fd62e7fb1ed987c77751b42ca05ebd6e62b247c0
manzieric <ericantwerpen@hotmail.com>
2023-04-04 15:46:29 +0200

    project setup

---
+++ components/FormComponent.js
@@ -0,0 +1,10 @@
+const FormComponent = class {
+  constructor() {}
+  start() {
+    // Start modifying the form elements here!
+    // You are allowed to add extra methods, properties or change the constructor of this class
+  }
+};
+
+const fc = new FormComponent();
+fc.start();
---
+++ data/oppoStatus.js
@@ -0,0 +1,27 @@
+export const oppoStatus = [
+  {
+    K_OPPO_STATUS: 1,
+    STATUS: "1. Initial Contact",
+    SUCCESS: 0,
+  },
+  {
+    K_OPPO_STATUS: 2,
+    STATUS: "2. Demonstration",
+    SUCCESS: 25,
+  },
+  {
+    K_OPPO_STATUS: 3,
+    STATUS: "3. Proposal",
+    SUCCESS: 50,
+  },
+  {
+    K_OPPO_STATUS: 4,
+    STATUS: "4. Negotiation",
+    SUCCESS: 75,
+  },
+  {
+    K_OPPO_STATUS: 5,
+    STATUS: "5. Order",
+    SUCCESS: 100,
+  },
+];
